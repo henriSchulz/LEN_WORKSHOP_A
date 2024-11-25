5f784e187e12c1e3f04932a73c3d90e55872d841
henrischulz <henri.schulz.bs@icloud.com>
2024-11-25 18:06:35 +0100

    add error handling and plot power

--- plot.py
+++ plot.py
@@ -3,30 +3,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-R_VALUES = [10, 100, 440, 680, 900, 1000, 1220, 1440, 1660, 2000, 3300, 4980]
+R_VALUES = [10, 100, 440, 680, 900, 1000, 1220, 1440, 1680, 2000, 3300, 4980]
 
 data_path = "data"
+
+
+def read_csv(file_path):
+    data = pd.read_csv(file_path, sep=";")
+    if data.empty:
+        raise ValueError(f"File {file_path} is empty")
+
+    return data
 
 
 # reads the data from the csv file and calculates the current and voltage values
 def process_measurements(src, resistance):
     file_path = os.path.join(data_path, f"{src}_{resistance}.csv")
     R_T = 10 ** 5
-    try:
-        data = pd.read_csv(file_path, sep=";")
-        # multiply every volatage by two
+    data = read_csv(file_path)
+    voltage = data.iloc[:, 2].mean() * 2
 
-        voltage = data.iloc[:, 2].mean() * 2
-        current = (voltage * (2 * R_T + resistance)) / (resistance * R_T)
-        power = voltage*current
+    # check if voltage is a number
 
-        return current, voltage, power
-    except Exception as e:
-        print(f"Failed to open file {file_path}. Error: {e}")
+    if np.isnan(voltage):
+        raise ValueError("Voltage is not a number for ", file_path)
+
+    current = (voltage * (2 * R_T + resistance)) / (resistance * R_T)
+    power = voltage * current
+
+    return current, voltage, power
 
 
-
-sources = ["informatikom", "schreibtischlampe75"]
+sources = ["schreibtischlampe75"]
 
 for src in sources:
     currents = []
@@ -38,10 +46,18 @@ for src in sources:
         voltages.append(voltage)
         powers.append(power)
     plt.plot(voltages, currents, marker="o")
+
     plt.xlabel("Spannung [V]")
     plt.ylabel("Strom [A]")
+
+    # add second y-axis for power
+
+    ax2 = plt.twinx()
+
+    ax2.plot(voltages, powers, marker="o", color="red")
+
+    ax2.set_ylabel("Leistung [W]")
+
     plt.title("U-I-Kennlinie")
     plt.grid()
     plt.savefig(f"plots/{src}.png")
-
-
