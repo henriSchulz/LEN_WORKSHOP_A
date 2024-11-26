84451513bc7e2d5390687d0ba395a1536f0b6cd2
henrischulz <henri.schulz.bs@icloud.com>
2024-11-26 17:50:05 +0100

    reorder code blocks

--- plot.py
+++ plot.py
@@ -15,15 +15,12 @@ def read_csv(file_path):
 
     return data
 
-
 # reads the data from the csv file and calculates the current and voltage values
 def process_measurements(src, resistance):
     file_path = os.path.join(data_path, f"{src}_{resistance}.csv")
     R_T = 10 ** 5
     data = read_csv(file_path)
     voltage = data.iloc[:, 2].mean() * 2
-
-    # check if voltage is a number
 
     if np.isnan(voltage):
         raise ValueError("Voltage is not a number for ", file_path)
@@ -34,59 +31,67 @@ def process_measurements(src, resistance):
     return current, voltage, power
 
 
-sources = ["schreibtischlampe75"]
+def save_latex_table(src, resistances, voltages, currents, powers):
+    if not os.path.isdir("latex"):
+        os.mkdir("latex")
 
-for src in sources:
-    currents = []
-    voltages = []
-    powers = []
-    for resistance in R_VALUES:
-        current, voltage, power = process_measurements(src, resistance)
-        currents.append(current)
-        voltages.append(voltage)
-        powers.append(power)
-    plt.plot(voltages, currents, marker="o")
-
-    max_power = max(powers)
-    max_power_index = powers.index(max_power)
-    max_power_voltage = voltages[max_power_index]
-    max_power_current = currents[max_power_index]
-
-    plt.xlabel("Spannung [V]")
-    plt.ylabel("Strom [A]")
-
-    plt.axvline(x=max_power_voltage, color="green", linestyle="--")
-    plt.axhline(y=max_power_current, color="green", linestyle="--")
+    with open(f"latex/{src}.tex", "w") as f:
+        latex_string = pd.DataFrame(
+            {"R": resistances, "V": voltages, "I": currents, "P": powers}
+        ).to_latex(index=False)
+        f.write(latex_string)
 
 
-    fig = plt.gcf()
+def plot_sources(sources):
+    for src in sources:
+        currents = []
+        voltages = []
+        powers = []
 
-    fig.set_size_inches(8, 6)
+        for resistance in R_VALUES:
+            current, voltage, power = process_measurements(src, resistance)
+            currents.append(current)
+            voltages.append(voltage)
+            powers.append(power)
 
-    plt.plot(max_power_voltage, max_power_current, marker="o", color="green")
+        save_latex_table(src, R_VALUES, voltages, currents, powers)
 
-    plt.legend(["U-I-Kennlinie", "max. Leistung"], loc="upper left")
+        plt.plot(voltages, currents, marker="o")
+
+        max_power = max(powers)
+        max_power_index = powers.index(max_power)
+        max_power_voltage = voltages[max_power_index]
+        max_power_current = currents[max_power_index]
+
+        plt.xlabel("Spannung [V]")
+        plt.ylabel("Strom [A]")
+
+        plt.axvline(x=max_power_voltage, color="green", linestyle="--")
+        plt.axhline(y=max_power_current, color="green", linestyle="--")
+
+        fig = plt.gcf()
+
+        fig.set_size_inches(8, 6)
+
+        plt.plot(max_power_voltage, max_power_current, marker="o", color="green")
+
+        plt.legend(["U-I-Kennlinie", "max. Leistung"], loc="upper left")
+
+        ax2 = plt.twinx()
+
+        ax2.plot(voltages, powers, marker="o", color="red")
+
+        ax2.set_ylabel("Leistung [W]")
+
+        plt.title("U-I-Kennlinie")
+        plt.grid(True)
 
 
-    #make plot larger for better readability
+
+        if not os.path.isdir("plots"):
+            os.mkdir("plots")
+        plt.savefig(f"plots/{src}.png")
 
 
-
-
-
-
-
-    # add second y-axis for power
-
-    ax2 = plt.twinx()
-
-    ax2.plot(voltages, powers, marker="o", color="red")
-
-    ax2.set_ylabel("Leistung [W]")
-
-    plt.title("U-I-Kennlinie")
-    plt.grid(True)
-
-    if not os.path.isdir("plots"):
-        os.mkdir("plots")
-    plt.savefig(f"plots/{src}.png")
+if __name__ == "__main__":
+    plot_sources(["schreibtischlampe75"])
